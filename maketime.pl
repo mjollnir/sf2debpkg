c43f857e612fbb43d2c079de51b3f4296d004d71
Penny Leach <penny@mjollnir.org>
2010-11-28 19:46:02 +0100

    finalised WORKING version of debian_templates and make file after packages installed

--- maketime.pl
+++ maketime.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
@@ -9,13 +10,14 @@ use warnings;
 use Carp;
 use FindBin;
 use File::Slurp;
+use File::Basename;
 use YAML qw(LoadFile);
 use lib "$FindBin::Bin/lib";
 use Liip::YAML;
 
 my $basedir = $FindBin::Bin;
 
-#Check dhms has already been run to generate a config.pl, 
+#Check dhms has already been run to generate a config.pl,
 #Config.pl should contain all the basic info needed to template files into place.
 #Except for package-specific config items which are in the yaml manifest
 unless ( -r 'config.pl' ) {
@@ -27,6 +29,7 @@ my %config;
 # Optionally pre-populate config options here, to be overridden by the config.pl (if set) eg:
 #$config{greeting} = 'hello world';
 %config = getconfig( %config );
+my $config = \%config;
 # Optionally override config options set in the package's config.pl here:
 #$config{companysponsor} = 'Catalyst IT LTD';
 
@@ -35,6 +38,35 @@ $appmanifest = Liip::YAML::multi_mergekeys($appmanifest);
 my $apps = $appmanifest->{apps};
 my @appnames = keys %$apps;
 foreach my $appname (@appnames) {
+}
+
+use constant TEMPLATE_DIR => 'packaging/debian_templates/';
+
+
+my $renamefiles = '';
+template_file(TEMPLATE_DIR . 'compat', 'debian/compat', $config);
+
+foreach my $appname (@appnames) {
+
+    $apps->{$appname}->{description} = $appname unless $apps->{$appname};
+
+    my $wantsweb = wants_profile($apps->{$appname}, 'web');
+    my $wantsdb = wants_profile($apps->{$appname}, 'db');
+    my $isstatic = wants_profile($apps->{$appname}, 'static');
+
+    if ($wantsdb) { #sanity check
+        unless ($apps->{$appname}->{dbconfig}
+            && $apps->{$appname}->{dbconfig}->{create}
+            && $apps->{$appname}->{dbconfig}->{dbtypes}
+        ) {
+            die("$appname wants a database but didn't provide any config dbconfig key, required: create (bool) and dbtypes (string, comma separated)");
+        }
+        $config{DBTYPES} = $apps->{$appname}->{dbconfig}->{dbtypes};
+        $config{DBCPREFIX} = $apps->{$appname}->{dbconfig}->{create} eq 'false' ? 'frontend.' : '';
+    }
+    my $destprefix = "debian/$config{PACKAGENAME}-$appname";
+
+##### first issue any prebuild commands if we have them
     if ($apps->{$appname}->{prebuild}) {
         if (ref $apps->{$appname}->{prebuild} eq 'ARRAY') {
             foreach my $command (@{$apps->{$appname}->{prebuild}}) {
@@ -44,166 +76,270 @@ foreach my $appname (@appnames) {
             system($apps->{$appname}->{prebuild});
         }
     }
-}
 
-use constant TEMPLATE_DIR => 'packaging/debian_templates/';
-# Get a list of files that need templating:
-my %templatefiles = list_template_files();
+##### dirs
+    my $dirsdata = template_file_string(TEMPLATE_DIR . 'dirs', $config, $appname);
+    $wantsweb && ($dirsdata .= template_file_string(TEMPLATE_DIR . 'dirs.web', $config, $appname));
+    write_file("$destprefix.dirs", $dirsdata);
 
-# Step through the listed files, templating them into TEMPLATE_DIR using variables from %config and the app if relevant
-foreach my $templatefile (keys %templatefiles) {
-    my $destination;
-    if ($templatefiles{$templatefile} == 1) {
-        foreach my $appname (@appnames) {
-            $destination = 'debian/' . $config{PACKAGENAME}  . '.' . $templatefile;
-            template_file(TEMPLATE_DIR . $templatefile, $destination, %config, $appname);
-        }
-    } else {
-        $destination = 'debian/' . $templatefile;
-        template_file(TEMPLATE_DIR . $templatefile, $destination, %config);
-    }
+##### install
+    my $installfile = create_installfile($config, $appname, $apps->{$appname});
+    write_file($destprefix . '.install', $installfile);
 
-}
-
-chmod 0744,'debian/rules';
-
-foreach my $appname (@appnames) {
-    my $installfile = create_installfile(%config, $appname, $apps->{$appname});
-    write_file('debian/' . $appname . '.install', $installfile);
+##### cron
     if ($apps->{$appname}->{cron}) {
         my $scheduling;
         my $cron_data;
         foreach my $command (keys %{$apps->{$appname}->{cron}}) {
             $scheduling = $apps->{$appname}->{cron}->{$command};
-            $cron_data.= "$scheduling www-data cd _-_WWWROOT_-_-$appname && php app/ $appname /console $command\n";
+            my $wwwroot = safe_wwwroot($config, $appname, 1);
+            $cron_data.= "$scheduling www-data cd $wwwroot-$appname && php app/$appname/console $command\n";
         }
-        write_file("debian/$config{PACKAGENAME}-$appname.cron.d", $cron_data);
+        write_file("$destprefix.cron.d", $cron_data);
+    }
+
+##### template
+    template_file(TEMPLATE_DIR . 'templates', "$destprefix.templates", $config, $appname);
+
+##### config
+    my $configdata = template_file_string(TEMPLATE_DIR . 'config', $config, $appname);
+    $wantsdb && ($configdata .= template_file_string(TEMPLATE_DIR . 'config.db', $config, $appname));
+    $wantsweb && ($configdata .= template_file_string(TEMPLATE_DIR . 'config.web', $config, $appname));
+    $isstatic || ($configdata .= template_file_string(TEMPLATE_DIR . 'config.dynamic', $config, $appname));
+
+    write_file($destprefix . '.config', $configdata);
+
+##### preinst
+    my $preinst = template_file_string(TEMPLATE_DIR . 'preinst', $config, $appname);
+    my $preinstweb = $wantsweb ?  template_file_string(TEMPLATE_DIR . 'preinst.web', $config, $appname) : '';
+    $preinst =~ s/__PREINST.WEB__/$preinstweb/;
+    write_file($destprefix . '.preinst', $preinst);
+
+##### postinst
+    my $postinst = template_file_string(TEMPLATE_DIR . 'postinst', $config, $appname);
+    my $postinstconstantsweb = $wantsweb ?  template_file_string(TEMPLATE_DIR . 'postinst.constants.web', $config, $appname) : '';
+    my $postinstconstantsdb = $wantsdb ?  template_file_string(TEMPLATE_DIR . 'postinst.constants.db', $config, $appname) : '';
+    my $postinstconfigureweb = $wantsweb ?  template_file_string(TEMPLATE_DIR . 'postinst.configure.web', $config, $appname) : '';
+    my $postinstconfigureendweb = $wantsweb ?  template_file_string(TEMPLATE_DIR . 'postinst.configure.end.web', $config, $appname) : '';
+    my $postinstconfiguredb = $wantsdb ?  template_file_string(TEMPLATE_DIR . 'postinst.configure.db', $config, $appname) : '';
+    my $postinstconfiguredynamic = !$isstatic ?  template_file_string(TEMPLATE_DIR . 'postinst.configure.dynamic', $config, $appname) : '';
+
+    $postinst =~ s/__POSTINST.CONSTANTS.WEB__/$postinstconstantsweb/;
+    $postinst =~ s/__POSTINST.CONSTANTS.DB__/$postinstconstantsdb/;
+    $postinst =~ s/__POSTINST.CONFIGURE.WEB__/$postinstconfigureweb/;
+    $postinst =~ s/__POSTINST.CONFIGURE.DB__/$postinstconfiguredb/;
+    $postinst =~ s/__POSTINST.CONFIGURE.DYNAMIC__/$postinstconfiguredynamic/;
+    $postinst =~ s/__POSTINST.CONFIGURE.END.WEB__/$postinstconfigureendweb/;
+
+    write_file($destprefix . '.postinst', $postinst);
+
+##### prerm
+    my $prerm = template_file_string(TEMPLATE_DIR . 'prerm', $config, $appname);
+    my $prermdb = $wantsdb ? template_file_string(TEMPLATE_DIR . 'prerm.db', $config, $appname) : '' ;
+    my $prermweb = $wantsweb ?  template_file_string(TEMPLATE_DIR . 'prerm.web', $config, $appname) : '';
+    $prerm =~ s/__PRERM.DB__/$prermdb/;
+    $prerm =~ s/__PRERM.WEB__/$prermweb/;
+    write_file($destprefix . '.prerm', $prerm);
+
+##### postrm
+    my $postrm = template_file_string(TEMPLATE_DIR . 'postrm', $config, $appname);
+    my $postrmdb = $wantsdb ? template_file_string(TEMPLATE_DIR . 'postrm.db', $config, $appname) : '' ;
+    $postrm =~ s/__POSTRM.DB__/$postrmdb/;
+    write_file($destprefix . '.postrm', $postrm);
+
+
+##### web only templates
+    if ($wantsweb) {
+        template_file(TEMPLATE_DIR . 'confmodule', "$destprefix.confmodule", $config, $appname);
+        template_file(TEMPLATE_DIR . 'logrotate', "$destprefix.logrotate", $config, $appname);
+        $config->{DOCROOTSUBDIR} = '';
+        if ($apps->{$appname}->{docrootsubdir}) {
+            $config->{DOCROOTSUBDIR} = $apps->{$appname}->{docrootsubdir};
+        }
+        foreach my $f qw(apache.conf.httpsonly apache.conf.http apache.conf.mixed apache.conf.redirect apachedir) {
+            template_file(TEMPLATE_DIR . $f . '.template', "$destprefix.$f.template", $config, $appname);
+        }
+    }
+
+##### special handling of frontend scripts
+##### dh_install can't handle renames, so we have to rename this file to index.php specially
+    if ($apps->{$appname}->{frontend}) {
+        $renamefiles .= "\n\t" . 'mv $(CURDIR)/debian/_-_PACKAGENAME_-_-' . $appname . '/_-_WWWROOTNS_-_-' . $appname . '/web/' . $apps->{$appname}->{frontend}  . ' $(CURDIR)/debian/_-_PACKAGENAME_-_-' . $appname . '/_-_WWWROOTNS_-_-' . $appname . '/web/index.php';
     }
 }
+
+$config->{RENAMEFILES} = $renamefiles;
+template_file(TEMPLATE_DIR . 'rules', 'debian/rules', $config);
+
+chmod 0744,'debian/rules';
 
 # control needs to be built up separately but in a different way
 build_control_file(\@appnames, $apps);
 
-# Create the file that tells the deb what files to put where
 
 # Build the package
 `dpkg-buildpackage -rfakeroot -us -uc`;
 
-`mv debian/changelog .`; # TODO all changelogs files
+`mv debian/changelog .`;
 `rm debian/* -Rf`;
-`mv changelog debian/`; # TODO all changelogs files
+`mv changelog debian/`;
 
 exit 0;
 
 # Template specified input file into a specified file, searching for strings which
-# 1 appear as keys in %config, and 
+# 1 appear as keys in %config, and
 # 2 are surrounded by the maketime templating marker "_-_",
 # then replacing them with the corrosponding value in %config
 # eg a file containing "Wanted to say _-_greeting_-_ from _-_packagename_-_"
 # might be copied into coderoot/debian as: "Wanted to say hello world from moodle-site-hogwarts"
+# If appname is specified, it is appended to the packagename, sitename, wwwroot and wwwrootns variables
 sub template_file {
-    my ($infile, $outfile, %subst, $appname) = @_;
+    my ($infile, $outfile, $subst, $appname) = @_;
+    my $data = template_file_string($infile, $subst, $appname);
+    write_file($outfile, $data);
+}
 
+# helper for template_file - returns the result as a string
+# rather than writing it out to a file.
+# for the arguments, see template_file comments.
+sub template_file_string {
+    my ($infile, $subst, $appname) = @_;
     my $data = read_file($infile);
     my $key;
-    foreach $key ( keys %subst ) {
-        my $value = $subst{$key};
-        if ($appname) {
+    if ($appname) {
+        $subst->{APPNAME} = $appname;
+    }
+    foreach $key ( keys %$subst ) {
+        my $value = $subst->{$key};
+        if ($appname && grep(/$key/, qw(PACKAGENAME SITENAME WWWROOT WWWROOTNS))) {
             $value .= '-' . $appname;
         }
         $value = '' unless defined $value;
         $data =~ s/_-_${key}_-_/$value/gxms;
     }
-    write_file($outfile, $data);
+    return $data;
 }
 
 # Purely an abstraction function
-# Called once to produce the debian install file - a list of files/directories that should be
+# Called for each application to produce the debian install file - a list of files/directories that should be
 # included in the deb, and where they should be copied on installation
 sub create_installfile {
-    my (%config, $appname, $app) = @_;
+    my ($config, $appname, $app) = @_;
     my $installfile;
 
     # Default place to ask the deb to put things on install.
-    my $safewwwroot = $config{WWWROOT} . '-' . $appname;
-    $safewwwroot =~ s/^\///;        # remove initial /
-    $safewwwroot =~ s/(.*)([^\/])$/$1$2\//;  # add trailing /
+    my $safewwwroot = safe_wwwroot($config, $appname);
+    my $destination = $config->{PACKAGENAME} . '-' . $appname;
 
-    if (grep('/^web$/', $app->{profiles})) {
+    if (wants_profile($apps->{$appname}, 'web')) {
         $installfile .= sprintf("%-30s %s\n",
-                'debian/apache.conf.redirect.template',
-                'usr/share/packaged-site/' . $config{PACKAGENAME} . '/');
+                "debian/$destination.apache.conf.redirect.template",
+                "usr/share/packaged-site/$destination/");
         $installfile .= sprintf("%-30s %s\n",
-                'debian/apache.conf.http.template',
-                'usr/share/packaged-site/' . $config{PACKAGENAME} . '/');
+                "debian/$destination.apache.conf.http.template",
+                "usr/share/packaged-site/$destination/");
         $installfile .= sprintf("%-30s %s\n",
-                'debian/apache.conf.httpsonly.template',
-                'usr/share/packaged-site/' . $config{PACKAGENAME} . '/');
+                "debian/$destination.apache.conf.httpsonly.template",
+                "usr/share/packaged-site/$destination/");
         $installfile .= sprintf("%-30s %s\n",
-                'debian/apache.conf.mixed.template',
-                'usr/share/packaged-site/' . $config{PACKAGENAME} . '/');
+                "debian/$destination.apache.conf.mixed.template",
+                "usr/share/packaged-site/$destination/");
         $installfile .= sprintf("%-30s %s\n",
-                'debian/apachedir.template',
-                'usr/share/packaged-site/' . $config{PACKAGENAME} . '/');
+                "debian/$destination.apachedir.template",
+                "usr/share/packaged-site/$destination/");
+        $installfile .= sprintf("%-30s %s\n",
+                "debian/$destination.confmodule",
+                "usr/share/packaged-site/$destination/");
     }
-    $installfile .= sprintf("%-30s %s\n",
-            'debian/confmodule',
-            'usr/share/packaged-site/' . $config{PACKAGENAME} . '/');
 
-
+    # each bundle needs to be copied
     if ($app->{bundles}) {
         foreach my $bundle (@{$app->{bundles}}) {
-            $installfile .= sprintf("%-30s %s\n", $bundle, $safewwwroot);
+            my ($filename, $directories) = fileparse($bundle);
+            $installfile .= sprintf("%-30s %s\n", $bundle, "$safewwwroot/$directories");
         }
     }
 
     # maybe it wants some other stuff too
     if ($app->{installfiles}) {
-        foreach my $installfile (@{$app->{installfiles}}) {
-            $installfile .= sprintf("%-30s %s\n", $installfile, $safewwwroot);
+        foreach my $file (@{$app->{installfiles}}) {
+            my ($filename, $directories) = fileparse($file);
+            $installfile .= sprintf("%-30s %s\n", $file, "$safewwwroot/$directories");
         }
     }
 
+    # front end script that is used to serve the application
     if ($app->{frontend}) {
-        $installfile .= sprintf("%-30s %s\n", $app->{frontend}, "$safewwwroot/index.php");
+        $installfile .= sprintf("%-30s %s\n", "web/$app->{frontend}", $safewwwroot . 'web/');
     }
 
+    # published assets (hopefully created during pre-build)
     if ($app->{assets}) {
-        #TODO assets
+        foreach my $asset (@{$app->{assets}}) {
+            $installfile .= sprintf("%-30s %s\n", $asset, $safewwwroot . 'web/');
+        }
+    }
+
+    # if we are using a dynamic.yml file, copy the one from the debian templates
+    # these should NOT get out of sync!
+    if  ( -e 'app/' . $appname . '/config/dynamic.yml') {
+        $installfile .= sprintf("%-30s %s\n",
+                    "packaging/debian_templates/dynamic.yml.template",
+                    "usr/share/packaged-site/$destination/");
     }
 
     return $installfile
 }
 
-# Purely an abstraction function:
-# Create an array of files that require templating into <coderoot>/debian
-sub list_template_files {
-    return (
-        #'control'                        => 0,
-        'compat'                         => 0,
-        'rules'                          => 0,
-        'dirs'                           => 1,
-        'config'                         => 1,
-        'templates'                      => 0,
-        'apache.conf.redirect.template'  => 0,
-        'apache.conf.http.template'      => 0,
-        'apache.conf.httpsonly.template' => 0,
-        'apache.conf.mixed.template'     => 0,
-        'apachedir.template'             => 0,
-        'preinst'                        => 1,
-        'postinst'                       => 1,
-        'postrm'                         => 1,
-        'prerm'                          => 1,
-        'confmodule'                     => 0,
-        'logrotate'                      => 1,
-    );
-}
-
+# build up the control file. start with the source package
+# and then for each application, append a binary package section
 sub build_control_file {
     # source package
     my ($appnames, $apps) = @_;
 
     # the template file already contains the source package declaration.
     # for each application we need to add a binary package declarataion from a template.
-    #TODO
+    my $control = template_file_string(TEMPLATE_DIR . 'control', $config);
+    my @controlsubst = qw(DEPENDENCIES CONFLICTS RECOMMENDS SUGGESTS PREDEPENDS);
+
+    foreach my $appname (@$appnames) {
+        my $app = $apps->{$appname};
+        foreach my $subst (@controlsubst) {
+            $config->{$subst} = '';
+            if ($app->{lc($subst)}) {
+                foreach my $package (keys %{$app->{lc($subst)}}) {
+                    $config->{$subst} .= $package;
+                    if ($app->{lc($subst)}->{$package}) {
+                        $config->{$subst} .= " ( $app->{lc($subst)}->{$package} )";
+                    }
+                    $config->{$subst} .= ' ,';
+                }
+                $config->{$subst} =~ s/.$//; # take off the last ,
+            }
+        }
+        $config->{DESCRIPTION} = $apps->{$appname}->{description};
+        if (wants_profile($app, 'db')) {
+            $config->{PREDEPENDS} .= ', dbconfig-common';
+        }
+        $control .= template_file_string(TEMPLATE_DIR . 'control.binarytemplate', $config, $appname);
+    }
+
+    write_file('debian/control', $control);
+}
+
+# helper function to return the safe wwwroot for templating
+sub safe_wwwroot {
+    my ($config, $appname, $absolute) = @_;
+    my $safewwwroot = $config->{WWWROOT} . '-' . $appname;
+    $absolute || $safewwwroot =~ s/^\///;        # remove initial /
+    $safewwwroot =~ s/(.*)([^\/])$/$1$2\//;  # add trailing /
+    return $safewwwroot;
+
+}
+
+# helper to check to see if the given app wants a specific profile
+sub wants_profile {
+    my $app = shift;
+    my $profile = shift;
+    return grep(/^$profile$/, @{$app->{profiles}});
 }
