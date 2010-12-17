99eb2f1a2b6a09f961a3a5664b52a4fec44fa343
Penny Leach <penny@mjollnir.org>
2010-12-17 15:08:23 +0100

    removed dependence on dh-make-site completely:
    - the changelog and Makefile are created from skeletons now by
      maketime.pl instead of dh-make-site, the first time it runs.
    
    - packagemanifest.yml now must contain the metadata that was previously
      inside config.pl from dh-make-site.  The skeleton for this is also
      created the first time maketime.pl is run.
    
    - When the packagemanifest is present but the debian/changelog is not,
      the skeleton contains slightly more sensible values based on the
      metadata in packagemanifest.yml

--- maketime.pl
+++ maketime.pl
@@ -9,46 +9,108 @@ use warnings;
 
 use Carp;
 use FindBin;
+use Cwd;
 use File::Basename;
 use File::Slurp;
+use File::Copy;
 use YAML qw(LoadFile);
 use lib "$FindBin::Bin/lib";
 use Liip::YAML;
 use Liip::Symfony2::Application;
 use Liip::Debian::Helper;
 
-my $basedir = "$FindBin::Bin/..";
+my $basedir = "$FindBin::Bin";
+my $cwd = getcwd();
 
-#Check dhms has already been run to generate a config.pl,
-#Config.pl should contain all the basic info needed to template files into place.
-#Except for package-specific config items which are in the yaml manifest
-unless ( -r 'config.pl' ) {
-    croak qq{Cannot read packaging/config.pl\n - Make sure you are in a site checkout and have run dh-make-site};
+if ($basedir eq $cwd) {
+    croak qq(Make sure this script is run from the base of your Symfony2 checkout);
 }
-require './config.pl';
 
-my %globalconfig;
-# Optionally pre-populate config options here, to be overridden by the config.pl (if set) eg:
-#$globalconfig{greeting} = 'hello world';
-# TODO replace with yml data
-%globalconfig = getconfig( %globalconfig );
-# Optionally override config options set in the package's config.pl here:
-#$globalconfig{companysponsor} = 'Catalyst IT LTD';
+my $ptemplatedir = "$cwd/packaging/project_templates/";
 
-my $appmanifest = LoadFile($basedir . '/packagemanifest.yml');
-$appmanifest = Liip::YAML::multi_mergekeys($appmanifest);
-my $appprofiles = $appmanifest->{apps};
+my $config = {};
+# if the packagemanifest exists, load it first so that we can create the changelog with helpful defaults
+if ( -r "$cwd/packagemanifest.yml") {
+    $config = LoadFile("$cwd/packagemanifest.yml");
+    $config = Liip::YAML::multi_mergekeys($config);
+}
+
+# basic dependencies to build the package are:
+# packagemanifest.yml
+# debian/changelog
+# Makefile
+my $dependenciesmissing = [];
+
+my $dependencies = {
+    Makefile => 'Makefile',
+    'packagemanifest.yml' => 'packagemanifest.yml',
+    'changelog' => 'debian/changelog',
+};
+
+foreach my $dep (keys %$dependencies) {
+    my $destination = $dependencies->{$dep};
+    next if (-r $destination);
+    copy("$ptemplatedir/$dep", "$cwd/$destination");
+    push @$dependenciesmissing, $dep;
+}
+
+# if any of these are not present, we can create skeletons and exit
+if (scalar @$dependenciesmissing gt 0) {
+    my $sensiblechangelog = 0;
+    if (scalar keys %$config && grep(/changelog/, @$dependenciesmissing)) {
+        $config->{project}->{projectname} && `sed -i "s/YOURPROJECT/$config->{project}->{projectname}/" debian/changelog`;
+        $config->{project}->{maintainername} && `sed -i "s/YOURNAME/$config->{project}->{maintainername}/" debian/changelog`;
+        $config->{project}->{maintaineremail} && `sed -i "s/YOU\@EXAMPLE.COM/$config->{project}->{maintaineremail}/" debian/changelog`;
+        $sensiblechangelog = 1;
+    }
+    print "Some dependencies were missing. I created examples for you:\n    ";
+    print join ("\n    ",  @{$dependencies}{@$dependenciesmissing});
+    print "\nPlease check them, commit them to your project, and try building again.\n";
+    if (grep(/Makefile/, @$dependenciesmissing)) {
+        print "Additionally, I created a Makefile for you, so you can now just run 'make' in your project root'\n";
+    }
+    if (!$sensiblechangelog && grep(/changelog/, @$dependenciesmissing)) {
+        print "Your debian/changelog in particular needs checking!\n";
+    }
+    exit;
+}
+
+# ok, if we've got this far, all the dependencies are present and we're good to go
+my $appprofiles = $config->{apps};
+my $globalconfig = $config->{project};
+
+unless ($appprofiles && $globalconfig) {
+    die "Misformed yml structure? apps: and project: must both be present!";
+}
+
+unless ($globalconfig->{projectname} && $globalconfig->{maintainername} && $globalconfig->{maintaineremail}) {
+    die "Misformed yaml structure? proejct: must contain projectname: and maintainername: and maintaineremail";
+}
+
+# backwards compatibility - all these used to be in the old config.pl, and are used as substvars:
+my $bcconfig = {
+    PACKAGENAME => 'symfony2-site-' . $globalconfig->{projectname},
+    SITETYPE    => 'symfony2',
+    SITENAME    => $globalconfig->{projectname},
+    WWWROOT     => '/var/www/symfony2-site-' . $globalconfig->{projectname},
+    WWWROOTNS   => 'var/www/symfony2-site-' . $globalconfig->{projectname},
+    FULLNAME    => $globalconfig->{maintainername},
+    EMAIL       => $globalconfig->{maintaineremail},
+};
+
+@{$globalconfig}{keys %$bcconfig} = values %$bcconfig;
+
 my $apps;
 
-my $renamefiles = '';
-my $helper = Liip::Debian::Helper->new(\%globalconfig);
+my $renamefiles = ''; # dh_install can't rename files, so this is done manually in debian/rules
+my $helper = Liip::Debian::Helper->new($globalconfig);
 
 $helper->template_file('compat', 'debian/compat', {foo => 'bar'});
 
 foreach my $appname (keys %$appprofiles) {
 
-    my $destprefix = "$globalconfig{PACKAGENAME}-$appname";
-    my $app = Liip::Symfony2::Application->new($appname, $appprofiles->{$appname}, $basedir, $helper, $destprefix);
+    my $destprefix = "$globalconfig->{PACKAGENAME}-$appname";
+    my $app = Liip::Symfony2::Application->new($appname, $appprofiles->{$appname}, $cwd, $helper, $destprefix);
 
     $app->sanity_check();
 
@@ -78,13 +140,13 @@ foreach my $appname (keys %$appprofiles) {
 
     $app->make_apache_config();
 
-    $apps->{$appname} = $app;
-
     $renamefiles .= $app->get_renames();
+
+    $apps->{$appname} = $app; # store this for building up debian/control
 }
 
-$globalconfig{RENAMEFILES} = $renamefiles;
-$helper->template_file('rules', 'debian/rules', \%globalconfig);
+$globalconfig->{RENAMEFILES} = $renamefiles;
+$helper->template_file('rules', 'debian/rules', $globalconfig);
 
 chmod 0744,'debian/rules';
 
